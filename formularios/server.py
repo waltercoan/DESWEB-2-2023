aa49ec3e4f0a2123d61c60f57c6fd113a8fcbaa0
WALTER SILVESTRE COAN <walter.s@univille.br>
2023-10-10 21:19:11 -0300

    servindo html pelo python

--- formularios/server.py
+++ formularios/server.py
@@ -1,9 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return '''
-           <h1>Eu nao acredito</h1> 
-           '''
+    return render_template('formulario.html')
+    #return '''
+    #       <h1>Eu nao acredito</h1> 
+    #       '''
