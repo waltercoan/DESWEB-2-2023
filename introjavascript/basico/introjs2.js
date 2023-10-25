3c2cda68a6b7a3ff159f9d8d3b67363ed3b43139
WALTER SILVESTRE COAN <walter.s@univille.br>
2023-10-25 20:42:00 -0300

    intro js

---
+++ introjavascript/basico/introjs2.js
@@ -0,0 +1,14 @@
+// setTimeout(function(){
+//     alert("Ola mundo...");
+// }, 1000);
+
+// var resp = confirm("Voce esta certo disso?");
+// alert(resp);
+
+// var resp = prompt("Informe sua data \
+// de nascimento", "dd/MM/aaaa");
+// alert(resp);
+
+//DOM Document Object Model
+window.document.write("Eu não acredito...");
+document.write("Pior que é verdade");
