3dd4f1350bfd39d59756de2afe3973503d4c58f0
WALTER SILVESTRE COAN <walter.s@univille.br>
2023-10-25 21:17:33 -0300

    intro js

---
+++ introjavascript/basico/introjs4.js
@@ -0,0 +1,13 @@
+a = 10;
+console.log(a + ' ' + typeof(a));
+a = 20.5;
+console.log(a + ' ' + typeof(a));
+a = true;
+console.log(a + ' ' + typeof(a));
+a = "Como assim a mesma variável"
+console.log(a + ' ' + typeof(a));
+a = ['laranja', 'banana', 'maça']
+console.log(a + ' ' + typeof(a));
+a = new Object();
+a.idade = 10;
+console.log(a.idade + ' ' + typeof(a));
